4cba223f7684744d5fdb702bde760a0c1d2a46e2
Alan Jefferson Nascimento <alan.jefferson@outlook.com>
2022-04-21 17:44:03 -0300

    Add project to repository

---
+++ CyclicRotation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CyclicRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _solution = new Solution();
+            int[] result = _solution.solution(new int[] { 3, 8, 9, 7, 6 }, 3);
+        }
+    }
+}
---
+++ CyclicRotation/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace CyclicRotation
+{
+	public class Solution
+	{
+		private int[] rotate(int[] B)
+		{
+			int[] rotatedB = B.ToArray();
+
+			for (int i = 0; i < B.Count(); i++) {
+				if (i == 0) {
+					rotatedB[i] = B[B.Count() - 1];
+				} else {
+					rotatedB[i] = B[i - 1];
+				}
+			}
+
+			return rotatedB;
+		}
+
+		public int[] solution(int[] A, int K)
+		{
+			int[] rotatedA = A;
+
+			for (int i = 1; i <= K; i++)
+				rotatedA = rotate(rotatedA);
+
+			return rotatedA;
+		}
+	}
+}
